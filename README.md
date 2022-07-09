944127559be57db7d86f81d1ef530a04979b123c
mamba111 <60425828+mamba111@users.noreply.github.com>
2022-07-09 14:22:17 +0100

    cypress
    E2E for front-end, most of this is glue code so if something went wrong cypress would find out

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 npm run build:watch
 open src/index.html
 ```
+
+## Test
+
+```bash
+npm install --global http-server
+cd src && http-server # in one tab
+npm run cypress:open
+npm test
+```
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,17 @@
+it('displays and filters transactions', () => {
+  cy.visit('http://127.0.0.1:8080')
+
+  cy.contains('loading', { matchCase: false })
+
+  cy.contains('Transaction History', { matchCase: false })
+
+  cy.get('table').find('tr').should('have.length', 21)
+
+  cy.get('select').select('10 Smallest Transactions')
+
+  cy.get('table').find('tr').should('have.length', 11)
+
+  cy.get('select').select('10 Biggest Transactions')
+
+  cy.get('table').find('tr').should('have.length', 11)
+})
